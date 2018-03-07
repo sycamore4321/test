077188747da8d950abf0aedaa35a8f865b7bccba
Omegattr <ttrkoza904@gmail.com>
2018-03-07 21:42:37 +0900

    180307

---
+++ HelloWorld.java
@@ -0,0 +1,34 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		
+		for(int i =0; i<3; i++ ){
+			System.out.println(i);
+		}
+		
+		
+		for(int a = 10; a>1; a-=3){
+			System.out.println(a);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
